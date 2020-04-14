1feab1cbd70473f70abdae3b4b16701d79fc8e0f
Husnoro <Husnoro>
2020-04-14 14:02:53 +0500

    создала класс

---
+++ description/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace description
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Student 
+    {
+        public string Name {get; set;}
+        public string LastName {get; set;}
+        public int grade{get; set;}
+        public int age {get; set;}
+        public Student (string Name, string LastName, int grade, int age)
+        {
+            this.Name = Name;
+            this.LastName = LastName;
+            this.grade = grade;
+            this.age = age;
+        }
+        public Student (){}
+        public void studentInfo()
+        {
+            System.Console.WriteLine($"Name: {Name}\nLastName: {LastName}\nStudent's grade: {grade}\nStudent's age {age}");
+        }
+    }
+}
